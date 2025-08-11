a95ca792ba0af9d62eaea1d6ccf60d166fdbf5b9
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-08-11 21:13:49 +0100

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,8 +1,14 @@
 // config/server.js
-module.exports = ({ env }) => ({
-  host: '0.0.0.0',
-  port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://api.effort-free.co.uk'),
-  proxy: true,
-  app: { keys: env.array('APP_KEYS') },   // <-- this line matters
-});
+module.exports = ({ env }) => {
+  const keys = env.array('APP_KEYS', [
+    'k1_default','k2_default','k3_default','k4_default'
+  ]);
+
+  return {
+    host: '0.0.0.0',
+    port: env.int('PORT', 1337),
+    url: env('PUBLIC_URL', 'https://api.effort-free.co.uk'),
+    proxy: true,
+    app: { keys },
+  };
+};
