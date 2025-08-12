c53d2ee621419de0b7170f3b5cd838c5ae7c5f41
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-08-12 22:55:47 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,15 +1,16 @@
 // config/database.js
 module.exports = ({ env }) => {
-  const databaseUrl = env('DATABASE_URL');
+  const ssl = env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: false } : false;
 
-  if (databaseUrl) {
+  if (env('DATABASE_URL')) {
     return {
       connection: {
         client: 'postgres',
         connection: {
-          connectionString: databaseUrl,
-          ssl: { rejectUnauthorized: false },
+          connectionString: env('DATABASE_URL'),
+          ssl,
         },
+        pool: { min: 0, max: 10 },
       },
     };
   }
@@ -20,11 +21,12 @@ module.exports = ({ env }) => {
       connection: {
         host: env('PGHOST', env('DATABASE_HOST', 'localhost')),
         port: env.int('PGPORT', env.int('DATABASE_PORT', 5432)),
-        database: env('PGDATABASE', env('DATABASE_NAME')),
-        user: env('PGUSER', env('DATABASE_USERNAME')),
-        password: env('PGPASSWORD', env('DATABASE_PASSWORD')),
-        ssl: env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: false } : false,
+        database: env('PGDATABASE', env('DATABASE_NAME', 'strapi')),
+        user: env('PGUSER', env('DATABASE_USERNAME', 'strapi')),
+        password: env('PGPASSWORD', env('DATABASE_PASSWORD', 'strapi')),
+        ssl,
       },
+      pool: { min: 0, max: 10 },
     },
   };
 };
