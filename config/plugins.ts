ce83470b06b9da5b89279ca7387d79b3f27f6731
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-08-20 15:21:10 +0100

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,2 +1,12 @@
 // config/plugins.ts
-export default () => ({});
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      register: {
+        // Extra fields you want to allow on /auth/local/register
+        allowedFields: ['preferredName', 'university', 'course', 'keyStats'],
+      },
+      jwt: { expiresIn: '7d' },
+    },
+  },
+});
