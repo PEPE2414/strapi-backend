1c79b1973f3b91b645b021b3be810b79693be4d2
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-08-31 18:20:41 +0100

    Update ats-term.ts

--- src/api/ats-term/services/ats-term.ts
+++ src/api/ats-term/services/ats-term.ts
@@ -1,4 +1,5 @@
 import { factories } from '@strapi/strapi';
+
 const UID = 'api::ats-term.ats-term' as const;
 
-export default factories.createCoreService(UID as any);
+export default (factories.createCoreService as any)(UID);
