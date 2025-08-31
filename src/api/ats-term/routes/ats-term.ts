f44027f5671ff2e62a69d1b9f3f7299885878438
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-08-31 18:10:52 +0100

    Update ats-term.ts

--- src/api/ats-term/routes/ats-term.ts
+++ src/api/ats-term/routes/ats-term.ts
@@ -1,6 +1,7 @@
 import { factories } from '@strapi/strapi';
+const UID = 'api::ats-term.ats-term' as const;
 
-export default factories.createCoreRouter('api::ats-term.ats-term', {
+export default factories.createCoreRouter(UID as any, {
   config: {
     find:    { auth: false },
     findOne: { auth: false },
