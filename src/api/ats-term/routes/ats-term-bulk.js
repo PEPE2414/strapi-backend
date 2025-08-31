1281cc6caa63ef3f4e1e846be71606b2ede1a773
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-08-31 10:09:38 +0100

    Update ats-term-bulk.js

--- src/api/ats-term/routes/ats-term-bulk.js
+++ src/api/ats-term/routes/ats-term-bulk.js
@@ -1,12 +1,11 @@
 'use strict';
-
 module.exports = {
   routes: [
     {
       method: 'POST',
       path: '/ats-terms/bulk',
       handler: 'api::ats-term.bulk.bulkUpsert',
-      config: { auth: false }, // protected by x-seed-secret header
+      config: { auth: false } // protected by x-seed-secret header
     },
   ],
 };
