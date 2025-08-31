bb037a4a54bddece9f8bbe1942acfd639628a90d
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-08-31 16:50:23 +0100

    Update xdebug.js

--- src/api/xdebug/controllers/xdebug.js
+++ src/api/xdebug/controllers/xdebug.js
@@ -1,4 +1,6 @@
-export default {
+'use strict';
+
+module.exports = {
   async index(ctx) {
     const cts = Object.keys(strapi.container.get('content-types') || {});
     ctx.body = { ok: true, contentTypes: cts.sort() };
