f0217cc92f3520ef36afc7e41c3474772119d20d
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-08-31 10:45:07 +0100

    Update ats-term.js

--- src/api/ats-term/controllers/ats-term.js
+++ src/api/ats-term/controllers/ats-term.js
@@ -2,4 +2,9 @@
 
 const { factories } = require('@strapi/strapi');
 
-module.exports = factories.createCoreController('api::ats-term.ats-term');
+module.exports = factories.createCoreController('api::ats-term.ats-term', ({ strapi }) => ({
+  // Debug endpoint: GET /api/ats-terms/ping
+  async ping(ctx) {
+    ctx.body = { ok: true, uid: 'api::ats-term.ats-term' };
+  },
+}));
