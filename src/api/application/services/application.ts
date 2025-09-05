674ec757a85b476169854f5a83e0b840f9b93b3d
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-05 11:29:56 +0100

    Update application.ts

--- src/api/application/services/application.ts
+++ src/api/application/services/application.ts
@@ -1,3 +1,3 @@
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::application.application');
+export default factories.createCoreService('api::application.application' as any); // ⟵ cast
