6c755c1b402863fe7f1b2b40dc900f7ef21e2c6d
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-05 20:25:10 +0100

    Update saved-job.ts

--- src/api/saved-job/routes/saved-job.ts
+++ src/api/saved-job/routes/saved-job.ts
@@ -2,14 +2,14 @@ export default {
   type: 'content-api',
   routes: [
     { method: 'GET',    path: '/saved-jobs',     handler: 'saved-job.find',
-      config: { middlewares: ['global::with-user'] } },
+      config: { auth: false, middlewares: ['global::with-user'] } },
     { method: 'GET',    path: '/saved-jobs/:id', handler: 'saved-job.findOne',
-      config: { middlewares: ['global::with-user'] } },
+      config: { auth: false, middlewares: ['global::with-user'] } },
     { method: 'POST',   path: '/saved-jobs',     handler: 'saved-job.create',
-      config: { middlewares: ['global::with-user'] } },
+      config: { auth: false, middlewares: ['global::with-user'] } },
     { method: 'PUT',    path: '/saved-jobs/:id', handler: 'saved-job.update',
-      config: { middlewares: ['global::with-user'] } },
+      config: { auth: false, middlewares: ['global::with-user'] } },
     { method: 'DELETE', path: '/saved-jobs/:id', handler: 'saved-job.delete',
-      config: { middlewares: ['global::with-user'] } }
-  ]
+      config: { auth: false, middlewares: ['global::with-user'] } },
+  ],
 };
