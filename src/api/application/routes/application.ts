844f6110b20fd99e0a19e0d309f4a9b00bd0a9f7
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-05 19:21:24 +0100

    Update application.ts

--- src/api/application/routes/application.ts
+++ src/api/application/routes/application.ts
@@ -1,14 +1,23 @@
 export default {
   type: 'content-api',
   routes: [
-    { method: 'GET',    path: '/applications',              handler: 'application.find',      config: { policies: ['global::is-authenticated'] } },
-    { method: 'GET',    path: '/applications/:id',          handler: 'application.findOne',   config: { policies: ['global::is-authenticated'] } },
-    { method: 'POST',   path: '/applications',              handler: 'application.create',    config: { policies: ['global::is-authenticated'] } },
-    { method: 'PUT',    path: '/applications/:id',          handler: 'application.update',    config: { policies: ['global::is-authenticated'] } },
-    { method: 'DELETE', path: '/applications/:id',          handler: 'application.delete',    config: { policies: ['global::is-authenticated'] } },
-    { method: 'GET',    path: '/applications/stats',        handler: 'application.stats',     config: { policies: ['global::is-authenticated'] } },
-    { method: 'GET',    path: '/applications/weekly',       handler: 'application.weekly',    config: { policies: ['global::is-authenticated'] } },
-    { method: 'POST',   path: '/applications/:id/transition', handler: 'application.transition', config: { policies: ['global::is-authenticated'] } },
-    { method: 'POST',   path: '/applications/:id/verify',   handler: 'application.verify',    config: { policies: ['global::is-authenticated'] } }
-  ]
+    { method: 'GET',    path: '/applications',               handler: 'application.find',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'GET',    path: '/applications/:id',           handler: 'application.findOne',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'POST',   path: '/applications',               handler: 'application.create',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'PUT',    path: '/applications/:id',           handler: 'application.update',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'DELETE', path: '/applications/:id',           handler: 'application.delete',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'GET',    path: '/applications/stats',         handler: 'application.stats',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'GET',    path: '/applications/weekly',        handler: 'application.weekly',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'POST',   path: '/applications/:id/transition',handler: 'application.transition',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'POST',   path: '/applications/:id/verify',    handler: 'application.verify',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+  ],
 };
