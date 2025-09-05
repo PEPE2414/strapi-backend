61be223aaddadcebf16ec1a11e9ef1627e97078e
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-05 12:44:38 +0100

    Update application.ts

--- src/api/application/routes/application.ts
+++ src/api/application/routes/application.ts
@@ -1,60 +1,14 @@
 export default {
-  type: 'content-api', // ✅ make this a content API route set
+  type: 'content-api',
   routes: [
-    {
-      method: 'GET',
-      path: '/applications',
-      handler: 'application.find',
-      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
-    },
-    {
-      method: 'GET',
-      path: '/applications/:id',
-      handler: 'application.findOne',
-      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
-    },
-    {
-      method: 'POST',
-      path: '/applications',
-      handler: 'application.create',
-      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
-    },
-    {
-      method: 'PUT',
-      path: '/applications/:id',
-      handler: 'application.update',
-      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
-    },
-    {
-      method: 'DELETE',
-      path: '/applications/:id',
-      handler: 'application.delete',
-      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
-    },
-    {
-      method: 'GET',
-      path: '/applications/stats',
-      handler: 'application.stats',
-      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
-    },
-    {
-      method: 'GET',
-      path: '/applications/weekly',
-      handler: 'application.weekly',
-      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
-    },
-    {
-      method: 'POST',
-      path: '/applications/:id/transition',
-      handler: 'application.transition',
-      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
-    },
-    {
-      method: 'POST',
-      path: '/applications/:id/verify',
-      handler: 'application.verify',
-      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
-    }
+    { method: 'GET',    path: '/applications',              handler: 'application.find',      config: { policies: ['global::is-authenticated'] } },
+    { method: 'GET',    path: '/applications/:id',          handler: 'application.findOne',   config: { policies: ['global::is-authenticated'] } },
+    { method: 'POST',   path: '/applications',              handler: 'application.create',    config: { policies: ['global::is-authenticated'] } },
+    { method: 'PUT',    path: '/applications/:id',          handler: 'application.update',    config: { policies: ['global::is-authenticated'] } },
+    { method: 'DELETE', path: '/applications/:id',          handler: 'application.delete',    config: { policies: ['global::is-authenticated'] } },
+    { method: 'GET',    path: '/applications/stats',        handler: 'application.stats',     config: { policies: ['global::is-authenticated'] } },
+    { method: 'GET',    path: '/applications/weekly',       handler: 'application.weekly',    config: { policies: ['global::is-authenticated'] } },
+    { method: 'POST',   path: '/applications/:id/transition', handler: 'application.transition', config: { policies: ['global::is-authenticated'] } },
+    { method: 'POST',   path: '/applications/:id/verify',   handler: 'application.verify',    config: { policies: ['global::is-authenticated'] } }
   ]
 };
-
