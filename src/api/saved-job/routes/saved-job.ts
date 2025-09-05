2e3b3558c6564b36e4f3d9a559f12a188ff7dffe
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-05 11:14:32 +0100

    Create saved-job.ts

---
+++ src/api/saved-job/routes/saved-job.ts
@@ -0,0 +1,34 @@
+export default {
+  routes: [
+    {
+      method: 'GET',
+      path: '/saved-jobs',
+      handler: 'saved-job.find',
+      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
+    },
+    {
+      method: 'GET',
+      path: '/saved-jobs/:id',
+      handler: 'saved-job.findOne',
+      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
+    },
+    {
+      method: 'POST',
+      path: '/saved-jobs',
+      handler: 'saved-job.create',
+      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
+    },
+    {
+      method: 'PUT',
+      path: '/saved-jobs/:id',
+      handler: 'saved-job.update',
+      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
+    },
+    {
+      method: 'DELETE',
+      path: '/saved-jobs/:id',
+      handler: 'saved-job.delete',
+      config: { policies: ['plugin::users-permissions.isAuthenticated'] }
+    }
+  ]
+};
