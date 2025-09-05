983f37761987bad6d82aa6380fa531fde0929adc
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-05 19:21:38 +0100

    Update saved-job.ts

--- src/api/saved-job/routes/saved-job.ts
+++ src/api/saved-job/routes/saved-job.ts
@@ -1,10 +1,15 @@
 export default {
   type: 'content-api',
   routes: [
-    { method: 'GET',    path: '/saved-jobs',       handler: 'saved-job.find',     config: { policies: ['global::is-authenticated'] } },
-    { method: 'GET',    path: '/saved-jobs/:id',   handler: 'saved-job.findOne',  config: { policies: ['global::is-authenticated'] } },
-    { method: 'POST',   path: '/saved-jobs',       handler: 'saved-job.create',   config: { policies: ['global::is-authenticated'] } },
-    { method: 'PUT',    path: '/saved-jobs/:id',   handler: 'saved-job.update',   config: { policies: ['global::is-authenticated'] } },
-    { method: 'DELETE', path: '/saved-jobs/:id',   handler: 'saved-job.delete',   config: { policies: ['global::is-authenticated'] } }
-  ]
+    { method: 'GET',    path: '/saved-jobs',     handler: 'saved-job.find',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'GET',    path: '/saved-jobs/:id', handler: 'saved-job.findOne',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'POST',   path: '/saved-jobs',     handler: 'saved-job.create',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'PUT',    path: '/saved-jobs/:id', handler: 'saved-job.update',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+    { method: 'DELETE', path: '/saved-jobs/:id', handler: 'saved-job.delete',
+      config: { middlewares: ['global::with-user'], policies: ['global::is-authenticated'] } },
+  ],
 };
