3e2f55ed2c2ab48612cff9cb90c2b8ddcf45a0f5
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-05 22:02:23 +0100

    Update saved-job.ts

--- src/api/saved-job/routes/saved-job.ts
+++ src/api/saved-job/routes/saved-job.ts
@@ -1,15 +1,10 @@
 export default {
   type: 'content-api',
   routes: [
-    { method: 'GET',    path: '/saved-jobs',     handler: 'saved-job.find',
-      config: { auth: false, middlewares: ['global::with-user'] } },
-    { method: 'GET',    path: '/saved-jobs/:id', handler: 'saved-job.findOne',
-      config: { auth: false, middlewares: ['global::with-user'] } },
-    { method: 'POST',   path: '/saved-jobs',     handler: 'saved-job.create',
-      config: { auth: false, middlewares: ['global::with-user'] } },
-    { method: 'PUT',    path: '/saved-jobs/:id', handler: 'saved-job.update',
-      config: { auth: false, middlewares: ['global::with-user'] } },
-    { method: 'DELETE', path: '/saved-jobs/:id', handler: 'saved-job.delete',
-      config: { auth: false, middlewares: ['global::with-user'] } },
+    { method: 'GET',    path: '/saved-jobs',     handler: 'saved-job.find',     config: { auth: false } },
+    { method: 'GET',    path: '/saved-jobs/:id', handler: 'saved-job.findOne',  config: { auth: false } },
+    { method: 'POST',   path: '/saved-jobs',     handler: 'saved-job.create',   config: { auth: false } },
+    { method: 'PUT',    path: '/saved-jobs/:id', handler: 'saved-job.update',   config: { auth: false } },
+    { method: 'DELETE', path: '/saved-jobs/:id', handler: 'saved-job.delete',   config: { auth: false } },
   ],
 };
