1a2eab3d615462e63d71560ca9cebd9451e466e9
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-14 20:15:36 +0100

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,31 +1,30 @@
 // config/plugins.ts
-export default ({ env }) => ({
-  'users-permissions': {
-    config: {
-      register: {
-        allowedFields: ['preferredName','university','course','studyField','keyStats'],
-      },
-      jwt: { expiresIn: '7d' },
-    },
-  },
+export default ({ env }) => {
+  const mode = env('UPLOAD_PROVIDER', 'local');
+  const isAws = mode === 'aws' || mode === 'aws-s3';
 
-  upload: {
-    // Use local in dev by default; switch to S3 in prod by setting UPLOAD_PROVIDER=aws
-    config: env('UPLOAD_PROVIDER', 'local') === 'aws'
-      ? {
-          provider: '@strapi/provider-upload-aws-s3',
-          providerOptions: {
-            accessKeyId: env('S3_ACCESS_KEY_ID'),
-            secretAccessKey: env('S3_SECRET_ACCESS_KEY'),
-            region: env('S3_REGION'),
-            params: { Bucket: env('S3_BUCKET') },
-          },
-          actionOptions: {
-            upload: {},
-            uploadStream: {},
-            delete: {},
-          },
-        }
-      : {},
-  },
-});
+  return {
+    'users-permissions': {
+      config: {
+        register: {
+          allowedFields: ['preferredName','university','course','studyField','keyStats'],
+        },
+        jwt: { expiresIn: '7d' },
+      },
+    },
+    upload: {
+      config: isAws
+        ? {
+            provider: '@strapi/provider-upload-aws-s3', // requires the package installed
+            providerOptions: {
+              accessKeyId: env('S3_ACCESS_KEY_ID'),
+              secretAccessKey: env('S3_SECRET_ACCESS_KEY'),
+              region: env('S3_REGION'),
+              params: { Bucket: env('S3_BUCKET') },
+            },
+            actionOptions: { upload: {}, uploadStream: {}, delete: {} },
+          }
+        : {}, // local (dev)
+    },
+  };
+};
