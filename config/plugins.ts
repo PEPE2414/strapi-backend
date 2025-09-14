ea679a4426ef27ed0badb8bc5eae6ea81ef5f44d
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-14 19:51:42 +0100

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,10 +3,29 @@ export default ({ env }) => ({
   'users-permissions': {
     config: {
       register: {
-        // Extra fields you want to allow on /auth/local/register
         allowedFields: ['preferredName','university','course','studyField','keyStats'],
       },
       jwt: { expiresIn: '7d' },
     },
+  },
+
+  upload: {
+    // Use local in dev by default; switch to S3 in prod by setting UPLOAD_PROVIDER=aws
+    config: env('UPLOAD_PROVIDER', 'local') === 'aws'
+      ? {
+          provider: '@strapi/provider-upload-aws-s3',
+          providerOptions: {
+            accessKeyId: env('S3_ACCESS_KEY_ID'),
+            secretAccessKey: env('S3_SECRET_ACCESS_KEY'),
+            region: env('S3_REGION'),
+            params: { Bucket: env('S3_BUCKET') },
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        }
+      : {},
   },
 });
