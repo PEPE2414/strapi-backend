1abca5eef6e3d6a1e6652529536d4d48b55c0006
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-17 20:29:38 +0100

    Update saved-job.ts

--- src/api/saved-job/routes/saved-job.ts
+++ src/api/saved-job/routes/saved-job.ts
@@ -1,10 +1,10 @@
 export default {
   type: 'content-api',
   routes: [
-    { method: 'GET',    path: '/saved-jobs',     handler: 'savedtracker.find',     config: { policies: [] },
-    { method: 'GET',    path: '/saved-jobs/:id', handler: 'savedtracker.findOne',  config: { policies: [] },
-    { method: 'POST',   path: '/saved-jobs',     handler: 'savedtracker.create',   config: { policies: [] },
-    { method: 'PUT',    path: '/saved-jobs/:id', handler: 'savedtracker.update',   config: { policies: [] },
-    { method: 'DELETE', path: '/saved-jobs/:id', handler: 'savedtracker.delete',   config: { policies: [] },
+    { method: 'GET',    path: '/saved-jobs',     handler: 'savedtracker.find', 
+    { method: 'GET',    path: '/saved-jobs/:id', handler: 'savedtracker.findOne', 
+    { method: 'POST',   path: '/saved-jobs',     handler: 'savedtracker.create',  
+    { method: 'PUT',    path: '/saved-jobs/:id', handler: 'savedtracker.update',  
+    { method: 'DELETE', path: '/saved-jobs/:id', handler: 'savedtracker.delete',  
   ],
 };
