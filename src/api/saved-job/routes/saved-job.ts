748ec10d8c57451fb3295c23213689d9b55cf9f4
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-17 20:38:27 +0100

    Update saved-job.ts

--- src/api/saved-job/routes/saved-job.ts
+++ src/api/saved-job/routes/saved-job.ts
@@ -1,48 +1,10 @@
 export default {
   type: 'content-api',
   routes: [
-    {
-      method: 'GET',
-      path: '/saved-jobs',
-      handler: 'savedtracker.find',
-      config: {
-        // Use the users-permissions policy to require a valid JWT
-        // and populate ctx.state.user. This works in v5 and doesn't
-        // trigger the build error you saw with auth:true.
-        policies: ['plugin::users-permissions.isAuthenticated'],
-      },
-    },
-    {
-      method: 'GET',
-      path: '/saved-jobs/:id',
-      handler: 'savedtracker.findOne',
-      config: {
-        policies: ['plugin::users-permissions.isAuthenticated'],
-      },
-    },
-    {
-      method: 'POST',
-      path: '/saved-jobs',
-      handler: 'savedtracker.create',
-      config: {
-        policies: ['plugin::users-permissions.isAuthenticated'],
-      },
-    },
-    {
-      method: 'PUT',
-      path: '/saved-jobs/:id',
-      handler: 'savedtracker.update',
-      config: {
-        policies: ['plugin::users-permissions.isAuthenticated'],
-      },
-    },
-    {
-      method: 'DELETE',
-      path: '/saved-jobs/:id',
-      handler: 'savedtracker.delete',
-      config: {
-        policies: ['plugin::users-permissions.isAuthenticated'],
-      },
-    },
+    { method: 'GET',    path: '/saved-jobs',     handler: 'savedtracker.find'     },
+    { method: 'GET',    path: '/saved-jobs/:id', handler: 'savedtracker.findOne'  },
+    { method: 'POST',   path: '/saved-jobs',     handler: 'savedtracker.create'   },
+    { method: 'PUT',    path: '/saved-jobs/:id', handler: 'savedtracker.update'   },
+    { method: 'DELETE', path: '/saved-jobs/:id', handler: 'savedtracker.delete'   },
   ],
 };
