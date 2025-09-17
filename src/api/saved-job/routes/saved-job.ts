f61189b978cee2b984521c3945b4e6595c9197f6
PEPE2414 <158167534+PEPE2414@users.noreply.github.com>
2025-09-17 19:10:42 +0100

    Update saved-job.ts

--- src/api/saved-job/routes/saved-job.ts
+++ src/api/saved-job/routes/saved-job.ts
@@ -1,10 +1,10 @@
 export default {
   type: 'content-api',
   routes: [
-    { method: 'GET',    path: '/saved-jobs',     handler: 'savedtracker.find',     config: { auth: false } },
-    { method: 'GET',    path: '/saved-jobs/:id', handler: 'savedtracker.findOne',  config: { auth: false } },
-    { method: 'POST',   path: '/saved-jobs',     handler: 'savedtracker.create',   config: { auth: false } },
-    { method: 'PUT',    path: '/saved-jobs/:id', handler: 'savedtracker.update',   config: { auth: false } },
-    { method: 'DELETE', path: '/saved-jobs/:id', handler: 'savedtracker.delete',   config: { auth: false } },
+    { method: 'GET',    path: '/saved-jobs',     handler: 'savedtracker.find',     config: { auth: true } },
+    { method: 'GET',    path: '/saved-jobs/:id', handler: 'savedtracker.findOne',  config: { auth: true } },
+    { method: 'POST',   path: '/saved-jobs',     handler: 'savedtracker.create',   config: { auth: true } },
+    { method: 'PUT',    path: '/saved-jobs/:id', handler: 'savedtracker.update',   config: { auth: true } },
+    { method: 'DELETE', path: '/saved-jobs/:id', handler: 'savedtracker.delete',   config: { auth: true } },
   ],
 };
