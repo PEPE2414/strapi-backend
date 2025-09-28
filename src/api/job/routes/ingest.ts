8fcb1eb24ab2d351170de69f6b2189968d8350fb
PEPE2414 <pepeortizflores@gmail.com>
2025-09-28 22:29:22 +0100

    chore: update from Cursor

--- src/api/job/routes/ingest.ts
+++ src/api/job/routes/ingest.ts
@@ -1,6 +1,24 @@
 export default {
   routes: [
-    { method: 'POST', path: '/jobs/ingest', handler: 'job.ingest', config: { auth: false } },
-    { method: 'GET',  path: '/jobs/recommendations', handler: 'job.recommendations', config: { auth: true } }
+    { 
+      method: 'POST', 
+      path: '/jobs/ingest', 
+      handler: 'job.ingest', 
+      config: { 
+        auth: false,
+        policies: [],
+        middlewares: []
+      } 
+    },
+    { 
+      method: 'GET', 
+      path: '/jobs/recommendations', 
+      handler: 'job.recommendations', 
+      config: { 
+        auth: false,
+        policies: [],
+        middlewares: []
+      } 
+    }
   ]
 };
