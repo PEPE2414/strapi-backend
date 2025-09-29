078469ee2fbfdb64af5c06ff3e15618b62286495
PEPE2414 <pepeortizflores@gmail.com>
2025-09-29 15:21:49 +0100

    chore: update from Cursor

--- jobs-ingest/src/config/sources.ts
+++ jobs-ingest/src/config/sources.ts
@@ -12,27 +12,21 @@ export const LEVER_COMPANIES = [
 ];
 
 export const MANUAL_URLS = [
-  // Specific high-volume job pages
-  'https://www.gradcracker.com/sitemap.xml',
-  'https://targetjobs.co.uk/sitemap.xml',
-  'https://www.prospects.ac.uk/sitemap.xml',
-  'https://uk.indeed.com/sitemap.xml',
+  // Working job board URLs only
   'https://www.reed.co.uk/sitemap.xml',
   'https://www.totaljobs.com/sitemap.xml',
-  'https://www.cv-library.co.uk/sitemap.xml',
   'https://www.monster.co.uk/sitemap.xml'
 ];
 
 export const SITEMAP_SOURCES = [
-  // Working job board sitemaps - let's try some different approaches
+  // Working job board sitemaps only
   'https://www.reed.co.uk/sitemap.xml',
   'https://www.totaljobs.com/sitemap.xml',
-  'https://www.monster.co.uk/sitemap.xml',
-  'https://www.jobsite.co.uk/sitemap.xml'
+  'https://www.monster.co.uk/sitemap.xml'
 ];
 
 // High-volume company career pages
 export const COMPANY_CAREER_SITEMAPS = [
-  'https://careers.atkinsglobal.com/sitemap.xml',
+  // Only working company career pages
   'https://careers.jacobs.com/sitemap.xml'
 ];
