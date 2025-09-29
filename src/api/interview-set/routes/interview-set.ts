c89ee36254c692cf2c86687714c8c66d8a4a319d
PEPE2414 <pepeortizflores@gmail.com>
2025-09-29 11:03:10 +0100

    chore: update from Cursor

--- src/api/interview-set/routes/interview-set.ts
+++ src/api/interview-set/routes/interview-set.ts
@@ -1,6 +1,6 @@
 export default {
   routes: [
-    { method: 'GET', path: '/interview-sets/me', handler: 'interview-set.listMine', config: { auth: true } },
-    { method: 'POST', path: '/interview-sets/generate', handler: 'interview-set.generate', config: { auth: true } }
+    { method: 'GET', path: '/interview-sets/me', handler: 'interview-set.listMine', config: { policies: ['global::is-authenticated'] } },
+    { method: 'POST', path: '/interview-sets/generate', handler: 'interview-set.generate', config: { policies: ['global::is-authenticated'] } }
   ]
 };
